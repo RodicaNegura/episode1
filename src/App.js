f5f9d6be51199fdc7a69bf0accf2375629911dc3
RodicaNegura <RodicaElenaNegura@gmail.com>
2023-10-17 15:02:58 +0100

    new if else

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import './App.css';
 
 function App() {
   const age = 16;
+  const isGreen = false;
 
   return (
     <div className="App">
       {age >= 18 ? <h1>Over Age</h1> : <h1>Under Age</h1>}
+      <h1 style={{color: isGreen ? "green" : "red"}}>This Has Color!</h1>
+
+      {isGreen && <button>This Is A Button!</button>}
     </div>
   );
 };
