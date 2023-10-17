d1f80448aa6ee0b1bbbe786fd3de7c8ecbeda3ad
RodicaNegura <RodicaElenaNegura@gmail.com>
2023-10-17 14:06:19 +0100

    if and else part

--- src/App.js
+++ src/App.js
@@ -1,43 +1,13 @@
-import styles from './App.module.css';
+import './App.css';
 
 function App() {
+  const age = 16;
+
   return (
-    <div className={styles.App}>
-      {/* <Job salary={75000} position="Senior SDE" company="Google"/>
-      <br></br>
-      <Job salary={85000} position="Junior SDE" company="Netflix"/>
-      <br></br>
-      <Job salary={95000} position="Project Manger" company="Amazon"/>
-      <br></br>
-      <br></br>
-      <AboutMe name="Rodica" nationality="romanian" age={32}/>
-      <br></br>
-      <AboutMe name="Mona" nationality="english" age={38}/>
-      <br></br>
-      <AboutMe name="dora" nationality="bulgarian" age={22}/> */}
-      <h1 className={styles.name}> Rodica </h1>
+    <div className="App">
+      {age >= 18 ? <h1>Over Age</h1> : <h1>Under Age</h1>}
     </div>
   );
 };
-
-// const AboutMe = (props) => {
-//   return (
-//     <div>
-//       <h1>{props.name}</h1>
-//       <h1>{props.nationality}</h1>
-//       <h1>{props.age}</h1>
-//     </div>
-//   );
-// };
-
-// const Job  = (props) => {
-//   return (
-//     <div>
-//       <h1>{props.company}</h1>
-//       <h2>{props.position}</h2>
-//       <h3>{props.salary}</h3>
-//     </div>
-//   );
-// };
 
 export default App;
