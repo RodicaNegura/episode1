cb4b09181184c245b15d36fb36e46775e5dc1128
RodicaNegura <RodicaElenaNegura@gmail.com>
2023-10-17 13:40:15 +0100

    practice new component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
 import './App.css';
 
 function App() {
-  const name = 'Rodica';
-  return <div className="App">{name}</div>;
-}
+  return (
+    <div className="App">
+      <Job salary={75000} position="Senior SDE" company="Google"/>
+      <br></br>
+      <Job salary={85000} position="Junior SDE" company="Netflix"/>
+      <br></br>
+      <Job salary={95000} position="Project Manger" company="Amazon"/>
+      <br></br>
+      <br></br>
+      <AboutMe name="Rodica" nationality="romanian" age={32}/>
+      <br></br>
+      <AboutMe name="Mona" nationality="english" age={38}/>
+      <br></br>
+      <AboutMe name="dora" nationality="bulgarian" age={22}/>
+    </div>
+  );
+};
+
+const AboutMe = (props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h1>{props.nationality}</h1>
+      <h1>{props.age}</h1>
+    </div>
+  );
+};
+
+const Job  = (props) => {
+  return (
+    <div>
+      <h1>{props.company}</h1>
+      <h2>{props.position}</h2>
+      <h3>{props.salary}</h3>
+    </div>
+  );
+};
 
 export default App;
