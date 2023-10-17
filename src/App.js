b44e1fdbd897ebe8eb6de4a198257d40addbc73e
RodicaNegura <RodicaElenaNegura@gmail.com>
2023-10-17 13:51:00 +0100

    rename app.css to app.module.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <Job salary={75000} position="Senior SDE" company="Google"/>
+    <div className={styles.App}>
+      {/* <Job salary={75000} position="Senior SDE" company="Google"/>
       <br></br>
       <Job salary={85000} position="Junior SDE" company="Netflix"/>
       <br></br>
@@ -14,29 +14,30 @@ function App() {
       <br></br>
       <AboutMe name="Mona" nationality="english" age={38}/>
       <br></br>
-      <AboutMe name="dora" nationality="bulgarian" age={22}/>
+      <AboutMe name="dora" nationality="bulgarian" age={22}/> */}
+      <h1 className={styles.name}> Rodica </h1>
     </div>
   );
 };
 
-const AboutMe = (props) => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h1>{props.nationality}</h1>
-      <h1>{props.age}</h1>
-    </div>
-  );
-};
+// const AboutMe = (props) => {
+//   return (
+//     <div>
+//       <h1>{props.name}</h1>
+//       <h1>{props.nationality}</h1>
+//       <h1>{props.age}</h1>
+//     </div>
+//   );
+// };
 
-const Job  = (props) => {
-  return (
-    <div>
-      <h1>{props.company}</h1>
-      <h2>{props.position}</h2>
-      <h3>{props.salary}</h3>
-    </div>
-  );
-};
+// const Job  = (props) => {
+//   return (
+//     <div>
+//       <h1>{props.company}</h1>
+//       <h2>{props.position}</h2>
+//       <h3>{props.salary}</h3>
+//     </div>
+//   );
+// };
 
 export default App;
