b98a662f83182c179ece7f17533352d3342ba881
RodicaNegura <RodicaElenaNegura@gmail.com>
2023-10-19 11:38:14 +0100

    useState example

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [ inputValue, setInputValue] = useState("");
+  const [ textColor, setTextColor] = useState("green");
 
-  const handleInputChange = (event) => {
-    setInputValue(event.target.value);
-  };
-
+ 
   return (
     <div className="App">
-      <input type='text' onChange={handleInputChange}/>
-      {inputValue}
+      <button 
+        onClick={() => {
+          setTextColor(textColor === "green" ? "red" : "green");
+        }}
+      >
+        Show/Hide
+      </button>
+      <h1 style={{color: textColor}}> Hi My Name is Rodica!</h1>
     </div>
   );
 };
